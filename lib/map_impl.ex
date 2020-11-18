20edf8161363ddaa546086cdce2bc213b18586b4
Adam Lancaster <lancaster28@hotmail.com>
2020-11-18 17:46:57 +0000

    it's all fucked WIP

--- lib/map_impl.ex
+++ lib/map_impl.ex
@@ -54,6 +54,7 @@ defimpl KeywordLens, for: Map do
   """
   def map_while(data, keyword_lens, fun) do
     Enum.reduce_while(keyword_lens, data, fn lens, acc ->
+      lens |> IO.inspect(limit: :infinity, label: "")
       lens_in(lens, acc, fun)
     end)
   end
@@ -81,18 +82,24 @@ defimpl KeywordLens, for: Map do
   end
 
   defp lens_in({key, value}, [current | acc], data, data_rest, fun) when is_list(value) do
+    "lens_in" |> IO.inspect(limit: :infinity, label: "2")
+    value |> IO.inspect(limit: :infinity, label: "VALUE")
     fetched = Map.fetch!(data, key)
     remaining = %{key => data_rest} |> Map.merge(Map.delete(data, key))
     lens_in(value, [[key | current] | acc], fetched, remaining, fun)
   end
 
   defp lens_in({key, value = {_, _}}, [current | acc], data, data_rest, fun) do
+    "lens_in" |> IO.inspect(limit: :infinity, label: "3")
+
     fetched = Map.fetch!(data, key)
     remaining = %{key => data_rest} |> Map.merge(Map.delete(data, key))
     lens_in(value, [[key | current] | acc], fetched, remaining, fun)
   end
 
   defp lens_in({key, value}, [acc | _], data, data_rest, fun) do
+    "lens_in" |> IO.inspect(limit: :infinity, label: "4")
+
     fetched =
       try do
         Map.fetch!(data, key)
@@ -112,13 +119,50 @@ defimpl KeywordLens, for: Map do
     end
   end
 
-  defp lens_in([{key, value}], [current | acc], data, data_rest, fun) when is_list(value) do
+  defp lens_in([{key, value}], [current | acc], data, data_rest, fun)
+       when is_list(value) or is_tuple(value) do
+    "lens_in" |> IO.inspect(limit: :infinity, label: "5")
     fetched = Map.fetch!(data, key)
     remaining = %{key => data_rest} |> Map.merge(Map.delete(data, key))
     lens_in(value, [[key | current] | acc], fetched, remaining, fun)
   end
 
+  defp lens_in([{key, value} | rest], [current | acc], data, data_rest, fun) do
+    "lens_in" |> IO.inspect(limit: :infinity, label: "6")
+
+    # Here we need to backtrack a bit
+    # We may have to implement step forward / step back.
+
+    # We first have to continue with value
+    fetched = Map.fetch!(data, key) |> IO.inspect(limit: :infinity, label: "FETCHED")
+    remaining = %{key => data_rest} |> Map.merge(Map.delete(data, key))
+
+    # This takes value to it's end and returns the changed stuff, much like below.
+    # So now we need to reverse
+    with {:cont, value_result} <- lens_in(value, [[key | current] | acc], fetched, remaining, fun) do
+      # Enum.reduce(Enum.reverse(current), value_result, fn key, acc -> Map.fetch!(acc, key) end)
+      # end
+      # with {:cont, result} <- fun.(fetched),
+      # {:cont, dataa} <- backtrack([key | current], [], result, remaining) do
+      data =
+        Enum.reduce(
+          Enum.reverse(current),
+          value_result |> IO.inspect(limit: :infinity, label: "VALUE RESULT"),
+          fn key, acc -> Map.fetch!(acc, key) end
+        )
+
+      lens_in(rest, [current | [[key | current] | acc]], data, data_rest, fun)
+    else
+      halt = {:halt, _} -> halt
+      _ -> raise KeywordLens.InvalidReducingFunctionError
+    end
+  end
+
+  # this seems wrong as value could be a tuple, and therefore not at the end of the path
+  # so the fetch would fail.
   defp lens_in([{key, value}], [current | _], data, data_rest, fun) do
+    "lens_in" |> IO.inspect(limit: :infinity, label: "7")
+
     fetched =
       try do
         Map.fetch!(data, key)
@@ -139,6 +183,7 @@ defimpl KeywordLens, for: Map do
   end
 
   defp lens_in([key], [current | _], data, data_rest, fun) do
+    "lens_in" |> IO.inspect(limit: :infinity, label: "8")
     fetched = Map.fetch!(data, key)
     remaining = %{key => data_rest} |> Map.merge(Map.delete(data, key))
 
@@ -150,8 +195,9 @@ defimpl KeywordLens, for: Map do
   end
 
   # Here we have gotten to the end of one path, so we need to finish it off, then
-  # move to the next path.
+  # move to the next path...
   defp lens_in([key | rest], [current | acc], data, data_rest, fun) do
+    "lens_in" |> IO.inspect(limit: :infinity, label: "9")
     fetched = Map.fetch!(data, key)
     remaining = %{key => data_rest} |> Map.merge(Map.delete(data, key))
 
@@ -165,11 +211,36 @@ defimpl KeywordLens, for: Map do
     end
   end
 
+  defp lens_in([], [acc = [key | _] | _], data, data_rest, fun) do
+    data_rest |> IO.inspect(limit: :infinity, label: "DATAA REST")
+    data |> IO.inspect(limit: :infinity, label: "DATAA")
+    key |> IO.inspect(limit: :infinity, label: "major key")
+
+    fetched =
+      Map.fetch!(data, key)
+      |> IO.inspect(limit: :infinity, label: "fetching")
+
+    remaining =
+      %{key => data_rest}
+      |> Map.merge(Map.delete(data, key))
+      |> IO.inspect(limit: :infinity, label: "remainin")
+
+    case fun.(fetched) |> IO.inspect(limit: :infinity, label: "") do
+      {:cont, result} -> backtrack(acc, [], result, remaining)
+      halt = {:halt, _} -> halt
+      _ -> raise KeywordLens.InvalidReducingFunctionError
+    end
+  end
+
   defp lens_in(key, [acc | _], data, data_rest, fun) do
+    "lens_in" |> IO.inspect(limit: :infinity, label: "10")
+    key |> IO.inspect(limit: :infinity, label: "KEYY")
+
     fetched = Map.fetch!(data, key)
+
     remaining = %{key => data_rest} |> Map.merge(Map.delete(data, key))
 
-    case fun.(fetched) do
+    case fun.(fetched) |> IO.inspect(limit: :infinity, label: "") do
       {:cont, result} -> backtrack([key | acc], [], result, remaining)
       halt = {:halt, _} -> halt
       _ -> raise KeywordLens.InvalidReducingFunctionError
