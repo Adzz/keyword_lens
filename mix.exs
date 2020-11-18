67b9039bdc27d6f0cf3e1cce982d66792ce61055
Adam Lancaster <lancaster28@hotmail.com>
2020-11-18 14:01:02 +0000

    Add exdoc for docs

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule KeywordLens.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev, runtime: false}]
   end
 
   defp description(), do: "A utility library for Ecto"
